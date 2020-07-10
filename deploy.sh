7b1f522134d3835b6e84cc8e5e2fdae25c112c2a
Gabriel Gómez <gabomgp4@gmail.com>
2020-07-10 03:55:12 -0500

    merde

--- deploy.sh
+++ deploy.sh
@@ -44,6 +44,26 @@ helm install nginx-ingress stable/nginx-ingress \
     --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux \
     --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux
 
+## Configuring subdomain with AKS using dynamic IP:
+
+# Public IP address of your ingress controller
+IP="MY_EXTERNAL_IP"
+
+# Name to associate with public IP address
+DNSNAME="demo-aks-ingress"
+
+# Get the resource-id of the public ip
+PUBLICIPID=$(az network public-ip list --query "[?ipAddress!=null]|[?contains(ipAddress, '$IP')].[id]" --output tsv)
+
+# Update public ip address with DNS name
+az network public-ip update --ids $PUBLICIPID --dns-name $DNSNAME
+
+# Display the FQDN
+az network public-ip show --ids $PUBLICIPID --query "[dnsSettings.fqdn]" --output tsv
+
+# Config regcred
+kubectl create secret docker-registry regcred --docker-server=ggomezakstesting.azurecr.io --docker-username=ggomezakstesting --docker-password="BKmixuI1xLkUPbL=foInrF7BASnE53Bx"
+
 #TODO: Explicar que se debe haber configurado las
 # * credenciales del registro docker
 # *
