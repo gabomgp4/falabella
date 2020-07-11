948badf2251a72aaed0ac3d4fd366a27f19f7fd4
Gabriel Gómez <gabomgp4@gmail.com>
2020-07-10 21:02:42 -0500

    Se corrige la tarea de instalación de docker

--- build.ps1
+++ build.ps1
@@ -17,12 +17,12 @@ properties {
 $registry_url = "$($registry.server).azurecr.io"
 
 Task InstallDocker {
-    apt-get update
-    apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+    sudo apt-get update
+    sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-    apt-get update
-    apt-get install docker-ce docker-ce-cli containerd.io
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    sudo apt-get update
+    sudo apt-get install docker-ce docker-ce-cli containerd.io
 }
 
 Task InstallHelm {
@@ -35,7 +35,7 @@ Task InstallAzureCli {
 
 
 Task InstallBinaries -Depends InstallDocker,InstallHelm,InstallAzureCli {
-
+    "install binaries"
 }
 
 
