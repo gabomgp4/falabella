0e3421ec9f4fb483c25247a40457eeda99c8efce
Gabriel Gómez <gabomgp4@gmail.com>
2020-07-10 21:06:30 -0500

    Se modifica la instalación de docker pa que no pida confirmación

--- build.ps1
+++ build.ps1
@@ -18,11 +18,11 @@ $registry_url = "$($registry.server).azurecr.io"
 
 Task InstallDocker {
     sudo apt-get update
-    sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+    sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
     sudo apt-get update
-    sudo apt-get install docker-ce docker-ce-cli containerd.io
+    sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 }
 
 Task InstallHelm {
